b5c092b0597771074fa6dd12d1ad1b088cddf91c
Jonathan Fieldsend <J.E.Fieldsend@exeter.ac.uk>
2018-07-12 12:44:37 +0100

    Added javadoc for methods in lon.examples interfaces

--- lons/examples/BinaryProblem.java
+++ lons/examples/BinaryProblem.java
@@ -2,24 +2,38 @@ package lons.examples;
 
 import lons.Problem;
 /**
- * Class defines additional methods that BinaryProblems must provide beyond those
- * defined in Problem
+ * Class defines additional static methods that BinaryProblem can use
  * 
  * @author Jonathan Fieldsend
  * @version 11/07/2018
  */
 public interface BinaryProblem extends Problem<BinarySolution>
 {
-    static final int[] bitPowers = BinaryProblem.getBitPowers(31);
+    static final int MAX_BITS_UNIQUE=31;
+    static final int[] bitPowers = BinaryProblem.getBitPowers(MAX_BITS_UNIQUE); //precompute for speed
+    
+    /**
+     * Converts array of booleans to an integer value. If array is longer than 31 elements this
+     * integer will not be unique across all solutions
+     * 
+     * @param x array to convert to integer value
+     * @return integer representing boolean array
+     */
     static int getIndexOfBitString(boolean[] x) {
         int index = 0;
-        for (int i=0; i<x.length; i++) 
+        for (int i=0; i<Math.max(x.length,MAX_BITS_UNIQUE); i++) 
             index += x[i] ? BinaryProblem.bitPowers[i] : 0;
         return index;  
     }
     
-    /*
-     * CHANGE TO MAKE SOLUTIONS WITH HASHCLASH
+    /**
+     * Converts an index to a corresponding bit string representation. Note that 
+     * cannot convert for lengths greater than 31, due to limit on size of Java int 
+     * type
+     * 
+     * @param index integer representation of bitstring to convert
+     * @param bitStringLength length of boolean array to generate
+     * @return array of booleans corrsponding to index value
      */
     static boolean[] getBitStringCorrespondingToIndex(int index, int bitStringLength) {
         boolean[] bitString = new boolean[bitStringLength];
@@ -34,14 +48,26 @@ public interface BinaryProblem extends Problem<BinarySolution>
         return bitString;
     }
     
+    /**
+     * Get array representing 2 raised to all non-negative integer values up to power 
+     * 
+     * @param power power to raise to
+     * @return array containing 2^0 up to 2^power
+     */
     static int[] getBitPowers(int power) {
         int[] x = new int[power];
         x[0] = 1;
-        for (int i = 1; i <= 30; i++)
+        for (int i = 1; i <= power; i++)
             x[i] = x[i-1] * 2;
         return x;
     }
     
+    /**
+     * Get value of 2 raised to power
+     * 
+     * @param power power to raise to
+     * @return 2^power value
+     */
     static int getBitPower(int power) {
         int v = 1;
         for (int i = 1; i <= power; i++)
--- lons/examples/BinarySolution.java
+++ lons/examples/BinarySolution.java
@@ -10,6 +10,18 @@ import lons.Solution;
  */
 public abstract class BinarySolution extends Solution
 {
+    /**
+     * Returns this solution as an array of booleans (may be shallow copy)
+     * 
+     * @return array of booleans representing bitstring solution
+     */
     public abstract boolean[] getDesignVector();
+    
+    /**
+     * Returns the value of this solution's index element as a booleans
+     * 
+     * @param index index of element to return
+     * @return booleans representing bit value of the index element
+     */
     public abstract boolean getDesignVariable(int index);
 }
